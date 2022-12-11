12290f8ef59ac66a0acd3d9f008ad8bed1bce8b9
Gabe616 <60316309+Gabe616@users.noreply.github.com>
2022-12-11 16:18:23 +0000

    yay

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
+-- quick hotfix
+local old
+old = hookfunction(game.HttpGet, function(a, b)
+    if b == "https://raw.githubusercontent.com/Gabe616/OCPlus/main/VERSION" then return "0.3-ALPHA" end
+
+    return _G.oldhttpget(a, b)
+end)
+_G.oldhttpget = _G.oldhttpget or old
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Gabe616/OCPlus/main/script.lua"), "=OCPlus.0.3-ALPHA")()
